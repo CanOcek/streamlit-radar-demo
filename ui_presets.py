15a7656abaf19e3cb53a7978ccf8f35ff09fa224
Can Öcek <c.oecek@house-of-communication-external.com>
2026-05-26 11:14:16 +0200

    Increased test company count to fit fuller DB

--- ui_presets.py
+++ ui_presets.py
@@ -4,12 +4,44 @@ Edit these lists to restrict which companies and categories appear in the
 sidebar. Leave a list empty to use all values loaded from the database.
 """
 
-PRESET_COMPANIES = [
+PRESET_COMPANIES_TEST = [
     "Epson",
     "Olympus",
     "Raiffeisen",
-    "Stiebel Eltron"
+    "Stiebel Eltron",
+    "BMW",
+
+    
 ]
+
+
+
+ALL_COMPANIES = [
+    #"AIDA",
+    "B. Braun",
+    "BMW",
+    "BNP Paribas",
+    "Berlin Airport",
+    "Dertour",
+    "ECE",
+    "EON",
+    #"EWie Einfach",
+    #"Ehrmann",
+    "Epson",
+    #"Greiner Bio One",
+    #"IHK",
+    "Lufthansa",
+    "Olympus",
+    "Penny",
+    "Raiffeisen",
+    #"SPD",
+    #"Scalable Capital",
+    #"Smart",
+    "Stiebel Eltron",
+    #"Stiftung Warentest",
+]
+
+PRESET_COMPANIES=ALL_COMPANIES
 
 PRESET_CATEGORIES = [
     "Financials",
